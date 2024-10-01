f2cd0d7ed0fee16ac8201e6d2f17b9f8d257ce22
OmniaAbdelraheem <tur52752@temple.edu>
2024-10-01 12:15:26 -0400

    Completed onClickListener Implemented the lambda body to launch new activity and pass value Extracted the transferred value and use for lyricsDisplayView text size

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -8,10 +8,11 @@ class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
+        val selectedTextSize = intent.getIntExtra("SELECTED_TEXT_SIZE", 0)
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            textSize = selectedTextSize.toFloat()
         }
 
 
